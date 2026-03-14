8315b95797695fe76d59105718140915743de74e
casperblac991 <casperblac099@gmail.com>
2026-03-14 22:01:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from services.dream_ai import analyze_dream_with_ai
 
 app = FastAPI()
 
+class DreamRequest(BaseModel):
+    dream: str
+
+
 @app.get("/")
 def home():
-    return {"message": "AI Dream Weaver platform running"}
+    return {
+        "message": "AI Dream Weaver platform running"
+    }
+
 
 @app.get("/api/status")
 def status():
-    return {"status": "server working"}
-
-from pydantic import BaseModel
-import os
-import requests
-
-class DreamRequest(BaseModel):
-    dream: str
+    return {
+        "status": "server working"
+    }
 
 
 @app.post("/api/analyze-dream")
@@ -23,39 +27,9 @@ def analyze_dream(data: DreamRequest):
 
     dream_text = data.dream
 
-    api_key = os.getenv("GEMINI_API_KEY")
-
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={api_key}"
-
-    prompt = f"""
-    Interpret this dream in a psychological and symbolic way:
-
-    Dream:
-    {dream_text}
-
-    Provide meaning and possible emotional interpretation.
-    """
-
-    payload = {
-        "contents":[
-            {
-                "parts":[
-                    {"text": prompt}
-                ]
-            }
-        ]
-    }
-
-    response = requests.post(url, json=payload)
-
-    result = response.json()
-
-    try:
-        text = result["candidates"][0]["content"]["parts"][0]["text"]
-    except:
-        text = "Dream interpretation unavailable."
+    interpretation = analyze_dream_with_ai(dream_text)
 
     return {
         "dream": dream_text,
-        "interpretation": text
+        "interpretation": interpretation
     }
