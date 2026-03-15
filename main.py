ee1d3ecf7610880e58ccb318d4bc1c3bcd198e1a
casperblac991 <casperblac099@gmail.com>
2026-03-15 10:36:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# الصفحة الرئيسية
+# تشغيل مجلد الصور والملفات الثابتة
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/")
 def home():
     return FileResponse("templates/index.html")
 
-# صفحات الموقع
-@app.get("/home")
-def home_page():
-    return FileResponse("templates/home.html")
-
-@app.get("/explore")
-def explore():
-    return FileResponse("templates/explore.html")
-
-@app.get("/search")
-def search():
-    return FileResponse("templates/search.html")
-
-@app.get("/trending")
-def trending():
-    return FileResponse("templates/trending.html")
-
-@app.get("/dream")
-def dream():
-    return FileResponse("templates/dream.html")
-
-@app.get("/dream-feed")
-def dream_feed():
-    return FileResponse("templates/dream_feed.html")
-
-@app.get("/history")
-def history():
-    return FileResponse("templates/history.html")
-
-@app.get("/dashboard")
-def dashboard():
-    return FileResponse("templates/dashboard.html")
-
-@app.get("/login")
-def login():
-    return FileResponse("templates/login.html")
-
-@app.get("/register")
-def register():
-    return FileResponse("templates/register.html")
-
-@app.get("/profile")
-def profile():
-    return FileResponse("templates/profile.html")
-
-# المدونة
 @app.get("/blog")
 def blog():
     return FileResponse("templates/blog/index.html")
@@ -76,4 +33,8 @@ def islamic():
 
 @app.get("/blog/snake-dream-meaning")
 def snake():
-    return File
+    return FileResponse("templates/blog/snake-dream-meaning.html")
+
+@app.get("/blog/flying-dream-meaning")
+def flying():
+    return FileResponse("templates/blog/flying-dream-meaning.html")
